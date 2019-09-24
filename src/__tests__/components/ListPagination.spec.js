a0cc7b3353de6d5e0c3bae782192ca3aefa3107b
Massimo Necchi <massimo@akkroo.com>
2019-09-24 10:56:47 +0100

    Installed @testing-library/react and created __tests__ folder

--- src/__tests__/components/ListPagination.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { getByTestId } from '@testing-library/dom';
-import ListPagination from '../../components/ListPagination';
-
-it('renders without crashing', () => {
-  const { container } = render(<ListPagination totalCount={810} sideItems={3} limit={25} offset={0} />);
-  const pagination = getByTestId(container, 'pagination');
-});
